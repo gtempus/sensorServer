ba479873fa44563a93cec190329428c0f7ce1e8a
Gary Tempus <gtempus@codegaloot.com>
2016-05-03 16:07:15 -0500

    Record temp in the Hub.

--- code.py
+++ code.py
@@ -9,7 +9,8 @@ render = web.template.render('templates/')
 urls = (
   '/', 'index',
   '/cycle', 'cycle',
-  '/ac', 'ac'
+  '/ac', 'ac',
+  '/hub_temp', 'hubTemp'
 )
 
 class index:
@@ -35,6 +36,18 @@ class ac:
     def POST(self):
         i = json.loads(web.data())
         n = db.insert('PumpAC', state=int(i['state']))
+        return n
+
+class hubTemp:
+    def GET(self):
+        temps = db.select('HubTemp')
+        return render.hub_temps(temps)
+
+    def POST(self):
+        i = json.loads(web.data())
+        temp = (int(i['state']) / 100.0 * 10) / 10
+        n = db.insert('HubTemp', state=temp)
+        return n;
 
 if __name__ == "__main__":
     app = web.application(urls, globals())

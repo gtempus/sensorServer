1a9bad31eacc930a5daafc5b649e2603128e7231
Gary Tempus <gtempus@codegaloot.com>
2016-05-03 12:14:46 -0500

    Add Pump AC sensor data.

--- code.py
+++ code.py
@@ -8,7 +8,8 @@ render = web.template.render('templates/')
 
 urls = (
   '/', 'index',
-  '/cycle', 'cycle'
+  '/cycle', 'cycle',
+  '/ac', 'ac'
 )
 
 class index:
@@ -17,10 +18,23 @@ class index:
         return render.cycles(pump_cycles)
 
 class cycle:
+    def GET(self):
+        pump_cycles = db.select('PumpCycle')
+        return render.cycles(pump_cycles)
+
     def POST(self):
         i = json.loads(web.data())
         n = db.insert('PumpCycle', state=int(i['state']))
         return n
+
+class ac:
+    def GET(self):
+        pump_ac = db.select('PumpAC')
+        return render.ac(pump_ac)
+
+    def POST(self):
+        i = json.loads(web.data())
+        n = db.insert('PumpAC', state=int(i['state']))
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
